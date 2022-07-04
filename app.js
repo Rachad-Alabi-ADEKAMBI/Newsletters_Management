8e63f79ec30964928a383eb06dd2c9a05503e996
Rachad ADEKAMBI <adekambirachad@gmail.com>
2022-07-05 01:00:16 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
 const { createApp } = Vue
-     createApp({
-        data() {
-            return {
-                newSubscriber:[
-                    /*
-                    { id: '1', name: 'appartement 1', price: '50 000', short_description: 'Lorem ipsum dolor sit amet consectetur adipisicing elitm  Lorem ipsum dolor sit amet consectetur adipisicing elitm ', rooms: '2', picture_1: 'https://www.xwegbe.com/wp-content/uploads/2022/05/appartement1-1.jpg'},
-                     */
-                ]
-            }
-            },
-                methods: {
-                    subscribeToNewsletters(){
-                    axios.get("https://xwegbe.com/wp-content/plugins/liste-appartements/model.php?action=getAllFreeAppartments").then(response  => 
-                        this.newSubscriber = response.data);
-                },
-                 toFormData(obj){
-                var  fd = new FormData();
-                for(var i in obj){
-                    fd.append(i, obj[i]);
-                }
-                return fd;
-            }
-            
-            }
- }).mount('#app')
+
+createApp({
+   data() {
+       return {
+         message: 'le msg'
+       }
+       },
+       mounted: function(){
+          // this.getAllAppartments();
+       },
+           methods: {
+               getAllAppartments(){
+               axios.get("https://mywp.solidarityworld.org/wp-content/plugins/liste-appartements/model.php?action=getAllAppartments").then(response => this.appartments = response.data);
+           }
+       }
+}).mount('#app')
--- subscribeToNewsletters.php
+++ subscribeToNewsletters.php
@@ -14,78 +14,92 @@
  * Text Domain:        Newsletters Management
  */
 
- add_shortcode('Newsletters Management', 'subscribeToNewsletters');
+ /*add_shortcode('Newsletters_Management', 'subscribeToNewsletters');
  wp_enqueue_script('vue', 'https://unpkg.com/vue@3');
  wp_enqueue_script('axios', 'https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js');
 
  function subscribeToNewsletters(){
     echo("
-    <div id='app'>  
-    <div class='newsletters overlay'>
-        <form action='model.php?action=subscribeToNewsletters' class='newsletters___form'
-            method='POST'>
-                        <h1 class=''>
-                            Inscription à la newsletters
-                        </h1>  <br>  
-
-
-                        <div class='row'>
-                            <div class='col'>
-                            <input type='text' class='form-control' v-model='newAppartment.name'
-                            placeholder='Nom de l'appartement'  name='name'>
-                            </div>
-                        
-                        </div> <br>
-
-                        <div class='row'>
-                        <div class='col-4'>
-                                <input type='text' class='form-control' name='first_name'
-                                  v-model='newSubscriber.first_name'
-                                placeholder='Prénoms' required>
-                                </div>
-
-                                <div class='col-4'>
-                                <input type='text' class='form-control' name='last_name'
-                                  v-model='newSubscriber.last_name'
-                                placeholder='Nom' required>
-                                </div>
-                        </div> <br>
-
-                        <div class='row'>
-                            <div class='col-12'>
-                                        <input type='text' class='form-control' name='country'
-                                        v-model='newSubscriber.country'
-                                        placeholder='Pays' required>
-                             </div>
-                        </div> <br>
-
-                        <div class='row'>
-                            <div class='col-12'>
-                                        <input type='text' class='form-control' name='email'
-                                        v-model='newSubscriber.email'
-                                        placeholder='Adresse mail' required>
-                             </div>
-                        </div> <br>
-
-                            <p>
-                                Notez que votre adresse mail ne sera jamais communiquée à autrui. 
-                                Vous ne serez pas non plus inondé de messages de notre part 
-                                (environ un mail hebdomadaire).
-
-                            </p>
-
-                            <button class='  v-on:click='subscribeToNewsletters();'>
-                                Inscription
-                            </button>
-        </form>
-    </div>
-</div>  ");
+      ");
       wp_enqueue_script('app', plugin_dir_url(__FILE__). 'app.js');
       wp_enqueue_style('bootstrap4', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css');
     wp_enqueue_style('', plugin_dir_url(__FILE__). 'style.css');
 
   
  }
- ?>
+ */
+
+add_shortcode ('Newsletters_Management', 'subscribeToNewsletters');
+ 
+wp_enqueue_script('vue', 'https://unpkg.com/vue@3');
+wp_enqueue_script('axios', 'https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js');
+
+function subscribeToNewsletters(){
+   echo("
+   <div id='app'>  
+   <div class='newsletters overlay'>
+       <form action='model.php?action=subscribeToNewsletters' class='newsletters___form'
+           method='POST'>
+                       <h1 class=''>
+                           Inscription à la newsletters
+                       </h1>  <br>  
+
+
+                       <div class='row'>
+                           <div class='col'>
+                           <input type='text' class='form-control' v-model='newAppartment.name'
+                           placeholder='Nom de l'appartement'  name='name'>
+                           </div>
+                       
+                       </div> <br>
+
+                       <div class='row'>
+                       <div class='col-4'>
+                               <input type='text' class='form-control' name='first_name'
+                                 v-model='newSubscriber.first_name'
+                               placeholder='Prénoms' required>
+                               </div>
+
+                               <div class='col-4'>
+                               <input type='text' class='form-control' name='last_name'
+                                 v-model='newSubscriber.last_name'
+                               placeholder='Nom' required>
+                               </div>
+                       </div> <br>
+
+                       <div class='row'>
+                           <div class='col-12'>
+                                       <input type='text' class='form-control' name='country'
+                                       v-model='newSubscriber.country'
+                                       placeholder='Pays' required>
+                            </div>
+                       </div> <br>
+
+                       <div class='row'>
+                           <div class='col-12'>
+                                       <input type='text' class='form-control' name='email'
+                                       v-model='newSubscriber.email'
+                                       placeholder='Adresse mail' required>
+                            </div>
+                       </div> <br>
+
+                           <p>
+                               Notez que votre adresse mail ne sera jamais communiquée à autrui. 
+                               Vous ne serez pas non plus inondé de messages de notre part 
+                               (environ un mail hebdomadaire).
+
+                           </p>
+
+                           <button class='  v-on:click='subscribeToNewsletters();'>
+                               Inscription
+                           </button>
+       </form>
+   </div>
+</div>
+   ");
+     wp_enqueue_script('app', plugin_dir_url(__FILE__). 'app.js');
+   wp_enqueue_style('', plugin_dir_url(__FILE__). 'style.css');
+ 
+} ?>
 
 
